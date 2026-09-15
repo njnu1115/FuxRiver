9db0e1efa70b668c2ff98bdeb46ce10f700b1acd
Chen L <njnu1115@gmail.com>
2026-09-15 01:49:31 +0000

    The last version with simple linear implementation

--- build.sh
+++ build.sh
@@ -1,3 +1,33 @@
-JAVA_HOME=/opt/android-studio/jbr ./gradlew assembleAndroidTest && \
-PATH=$PATH:/media/some/opt/android-sdk/platform-tools adb install -r ./build/outputs/apk/androidTest/debug/FuxRiver-debug-androidTest.apk && \
-/media/some/opt/android-sdk/platform-tools/adb shell am instrument -w -m -e class cn.demo.xriver.AlipaySignInTest#testAlipaySignIn cn.demo.xriver.test/androidx.test.runner.AndroidJUnitRunner
+#!/bin/bash
+
+export JAVA_HOME='/home/linuxbrew/.linuxbrew/Cellar/openjdk@17/17.0.20.1'
+export ANDROID_HOME=/mnt/nas2/CharLIU/opt/android/sdk
+export PATH=$PATH:/mnt/nas2/CharLIU/opt/android/sdk/platform-tools
+
+# export ANDROID_SERIAL=SGD6IZGQGUIF9LZL
+# export ANDROID_SERIAL=662846a7
+export ANDROID_SERIAL=localhost:33333
+
+
+cleanup() {
+    echo ""
+    echo ">>> 捕获到 Ctrl+C 信号，正在执行清理操作..."
+    
+    # 执行你需要的 adb 命令
+    adb shell am force-stop cn.demo.xriver.test
+    
+    echo ">>> 清理完成，准备退出脚本。"
+    
+    # 2. 退出脚本 (非常重要，否则脚本会继续往下执行)
+    exit 0 
+}
+
+# 使用 trap 捕获 SIGINT (Ctrl+C) 信号，并绑定到 cleanup 函数
+trap cleanup SIGINT
+
+echo "脚本已启动，正在运行中... (按 Ctrl+C 停止)"
+echo "目标应用: cn.demo.xriver.test"
+
+./gradlew assembleAndroidTest
+adb install -r ./build/outputs/apk/androidTest/debug/FuxRiver-debug-androidTest.apk
+adb shell am instrument -w -m -e class cn.demo.xriver.AlipaySignInTest#testAlipaySignIn cn.demo.xriver.test/androidx.test.runner.AndroidJUnitRunner
--- src/androidTest/java/cn/demo/xriver/AlipaySignInTest.java
+++ src/androidTest/java/cn/demo/xriver/AlipaySignInTest.java
@@ -30,263 +30,274 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class AlipaySignInTest {
 
-    private static final String ALIPAY_PACKAGE = "com.eg.android.AlipayGphone";
-    private static final String ALIPAY_SIGN_IN_ACTIVITY = "com.eg.android.AlipayGphone/com.alipay.mobile.nebulax.xriver.activity.XRiverActivity";
-    private static final String DEEP_LINK_URL = "alipays://platformapi/startapp?appId=68687805&url=https%3A%2F%2Frender.alipay.com%2Fp%2Fyuyan%2F180020380000000023%2Fpoint-sign-in.html";
-    private static final int LOOP_COUNT = 64;
-    private static final long WAIT_TIMEOUT = 1000; // ms
-    private static final String DEVICE_NAME = android.os.Build.DEVICE;
-    private static final int GO_FINISH_Y = DEVICE_NAME.equals("umi") ? 1600 : DEVICE_NAME.equals("rubens") ? 1900 : 1900;
-    private static final String[] clickTexts = {
-            "一键核算用电成本",
-            "从支付宝首页访问会员",
-            "合理规划用电开销",
-            "天天签到赢奖励",
-            "打卡签到领奖励",
-            "打卡记录每天好心情",
-            "智能算电省钱有道",
-            "浏览机汤租机3秒",
-            "浏览爱租相机3秒",
-            "浏览租机猩3秒",
-            "浏览网商贷15秒",
-            "用电省钱精准算费",
-            "电费明细精准呈现",
-            "看5秒视频领积分",
-            "观看15秒视频领积分",
-            "逛15秒安全知识",
-            "逛15秒支付有礼领红包",
-            "逛15秒芝麻租赁频道",
-            "逛15秒芝麻租赁首页",
-            "逛一逛乐游记",
-            "逛一逛余额宝",
-            "逛一逛余额宝摇钱树",
-            "逛一逛余额宝攒钱节",
-            "逛一逛摇红包",
-            "逛一逛支付宝运动路线",
-            "逛一逛每日惊喜不断",
-            "逛一逛福气鱼塘",
-            "逛一逛签到领红包",
-            "逛一逛芝麻信用",
-            "逛一逛芭芭农场",
-            "逛一逛蚂蚁新村",
-            "逛一逛蚂蚁森林",
-            "逛一逛话费活动",
-            "逛一逛领取优惠",
-            "逛一逛领奖励",
-            "逛一逛高德打车小程序",
-            "逛双11会场",
-            "逛大额账单",
-            "逛我的快递包裹游历",
-            "逛支付有礼每日攒红包",
-            "逛热卖好货15秒",
-            "逛蚂蚁庄园喂小鸡",
-            "逛退款账单",
-            "逛飞猪一日游景点门票",
-            "集鸿运金抢兑红包"
-    };
-    private static final String[] scrollTexts = {
-            "滑动浏览优品会场15秒",
-            "逛热卖好货15秒",
-            "逛15秒精选超值好物",
-            "滑动浏览15秒红包会场"
-    };
-    private static final String[] awayBackTexts = {
-            "逛一逛游戏中心",
-            "逛一逛淘宝斗地主",
-            "逛一逛淘宝消消乐",
-            "逛一逛淘宝芭芭农场",
-            "逛一逛淘宝视频",
-            "逛一逛淘金币频道",
-            "逛一逛小米钱包APP",
-            "逛一逛大众点评",
-            "逛淘宝签到领现金"
-    };
+	private static final String ALIPAY_PACKAGE = "com.eg.android.AlipayGphone";
+	private static final String ALIPAY_SIGN_IN_ACTIVITY = "com.eg.android.AlipayGphone/com.alipay.mobile.nebulax.xriver.activity.XRiverActivity";
+	private static final String DEEP_LINK_URL = "alipays://platformapi/startapp?appId=68687805&url=https%3A%2F%2Frender.alipay.com%2Fp%2Fyuyan%2F180020380000000023%2Fpoint-sign-in.html";
+	private static final int LOOP_COUNT = 64;
+	private static final long WAIT_TIMEOUT = 1000; // ms
+	private static final String DEVICE_NAME = android.os.Build.DEVICE;
+	private static final int GO_FINISH_Y = DEVICE_NAME.equals("umi") ? 1600 : DEVICE_NAME.equals("rubens") ? 2085 : 2085;
+	private static final String[] clickTexts = {
+			"一键核算用电成本",
+			"从支付宝首页访问会员",
+			"合理规划用电开销",
+			"天天签到赢奖励",
+			"打卡签到领奖励",
+			"打卡记录每天好心情",
+			"智能算电省钱有道",
+			"浏览机汤租机3秒",
+			"浏览爱租相机3秒",
+			"浏览租机猩3秒",
+			"浏览网商贷15秒",
+			"用电省钱精准算费",
+			"电费明细精准呈现",
+			"看5秒视频领积分",
+			"观看15秒视频领积分",
+			"逛15秒安全知识",
+			"逛15秒支付有礼领红包",
+			"逛15秒芝麻租赁频道",
+			"逛15秒芝麻租赁首页",
+			"逛一逛乐游记",
+			"逛一逛里程币兑红包",
+			"来余额宝攒钱节领红包",
+			"逛一逛滴滴出行活动",
+			"逛一逛余额宝",
+			"逛一逛余额宝摇钱树",
+			"逛一逛余额宝攒钱节",
+			"逛一逛摇红包",
+			"逛一逛支付宝运动路线",
+			"逛一逛支付有礼",
+			"逛一逛每日惊喜不断",
+			"逛一逛福气鱼塘",
+			"逛一逛签到领红包",
+			"逛一逛芝麻信用",
+			"逛一逛芭芭农场",
+			"逛一逛蚂蚁新村",
+			"逛一逛蚂蚁森林",
+			"逛一逛话费活动",
+			"逛一逛领取优惠",
+			"逛一逛领奖励",
+			"逛一逛高德打车小程序",
+			"逛双11会场",
+			"逛大额账单",
+			"逛我的快递包裹游历",
+			"逛支付有礼每日攒红包",
+			"逛热卖好货15秒",
+			"每日浇水领真绿植",
+//			"去抽金条得真花",
+			"逛蚂蚁庄园喂小鸡",
+			"逛退款账单",
+			"逛飞猪一日游景点门票",
+			"集鸿运金抢兑红包"
+	};
+	private static final String[] scrollTexts = {
+			"滑动浏览优品会场15秒",
+			"逛热卖好货15秒",
+			"逛15秒精选超值好物",
+			"逛一逛国补好货会场",
+			"滑动浏览15秒红包会场"
+	};
+	private static final String[] awayBackTexts = {
+			"逛一逛游戏中心",
+			"逛一逛淘宝斗地主",
+			"逛一逛淘宝消消乐",
+			"逛一逛淘宝芭芭农场",
+			"逛一逛淘宝视频",
+			"逛一逛淘金币频道",
+			"逛一逛小米钱包APP",
+			"逛一逛大众点评",
+			"逛淘宝签到领现金"
+	};
 
-    private UiDevice device;
-    private Context context;
+	private UiDevice device;
+	private Context context;
 
-    @Before
-    public void setUp() throws Exception {
-        device = UiDevice.getInstance(getInstrumentation());
-        context = getInstrumentation().getContext();
-        device.setOrientationNatural();
+	@Before
+	public void setUp() throws Exception {
+		device = UiDevice.getInstance(getInstrumentation());
+		context = getInstrumentation().getContext();
+		device.setOrientationNatural();
 
-        device.waitForWindowUpdate(ALIPAY_PACKAGE, WAIT_TIMEOUT);
-    }
+		device.waitForWindowUpdate(ALIPAY_PACKAGE, WAIT_TIMEOUT);
+	}
 
-    private boolean seekAndClick(String text) throws Exception {
-        Thread.sleep(WAIT_TIMEOUT);
-        UiObject2 obj = device.findObject(By.text(text));
-        if (obj != null) {
-            logger("Found and clicked: " + text);
-            obj.click();
-            Thread.sleep(WAIT_TIMEOUT);
-            return true;
-        } else {
-            logger("Not found or not clickable: " + text);
-            Thread.sleep(WAIT_TIMEOUT);
-            return false;
-        }
-    }
+	private boolean seekAndClick(String text) throws Exception {
+		Thread.sleep(WAIT_TIMEOUT);
+		UiObject2 obj = device.findObject(By.text(text));
+		if (obj != null) {
+			logger("Found and clicked: " + text);
+			obj.click();
+			Thread.sleep(WAIT_TIMEOUT);
+			return true;
+		} else {
+			logger("Not found or not clickable: " + text);
+			Thread.sleep(WAIT_TIMEOUT);
+			return false;
+		}
+	}
 
-    private void justChange() throws Exception {
-        int screenHeight = device.getDisplayHeight();
-        int screenWidth = device.getDisplayWidth();
-        int centerX = screenWidth / 2;
-        boolean isClicked = false;
-        for (int j = 0; j < 4; j++) {
-            UiObject2 obj = device.findObject(By.text("换一换"));
-            if (obj != null) {
-                obj.click();
-                isClicked = true;
-                // swipe to top and click "赚更多积分"
-                device.swipe(540, 300, 540, 1500, 50);
-                Thread.sleep(WAIT_TIMEOUT);
-                seekAndClick("赚更多积分");
-                break;
-            } else {
-                device.swipe(centerX, screenHeight * 3 / 4, centerX, screenHeight * 2 / 4, 200);
-            }
-        }
-        if (!isClicked) {
-            // Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEEP_LINK_URL));
-            // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            // context.startActivity(intent);
-        }
-    }
+	private void justChange() throws Exception {
+		int screenHeight = device.getDisplayHeight();
+		int screenWidth = device.getDisplayWidth();
+		int centerX = screenWidth / 2;
+		boolean isClicked = false;
+		for (int j = 0; j < 4; j++) {
+			UiObject2 obj = device.findObject(By.text("换一换"));
+			if (obj != null) {
+				obj.click();
+				isClicked = true;
+				// swipe to top and click "赚更多积分"
+				device.swipe(540, 300, 540, 1500, 50);
+				Thread.sleep(WAIT_TIMEOUT);
+				seekAndClick("赚更多积分");
+				break;
+			} else {
+				device.swipe(centerX, screenHeight * 3 / 4, centerX, screenHeight * 2 / 4, 200);
+			}
+		}
+		if (!isClicked) {
+			// Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEEP_LINK_URL));
+			// intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			// context.startActivity(intent);
+		}
+	}
 
-    private void ScrollTask(UiObject2 obj) throws Exception {
-        obj.click();
-        Thread.sleep(WAIT_TIMEOUT);
-        device.click(561, GO_FINISH_Y);
-        for (int j = 0; j < 9; j++) {
-            Thread.sleep(WAIT_TIMEOUT);
-            UiObject2 riskobj = device.findObject(By.text("账号风险检测"));
-            if (riskobj != null) {
-                device.pressBack();
-                continue;
-            }
-            Thread.sleep(WAIT_TIMEOUT);
-            device.swipe(561, 1000, 498, 800, 64);
-            Thread.sleep(WAIT_TIMEOUT);
-        }
-        device.pressBack();
-    }
+	private void ScrollTask(UiObject2 obj) throws Exception {
+		obj.click();
+		Thread.sleep(WAIT_TIMEOUT);
+		device.click(561, GO_FINISH_Y);
+		for (int j = 0; j < 9; j++) {
+			Thread.sleep(WAIT_TIMEOUT);
+			UiObject2 riskobj = device.findObject(By.text("账号风险检测"));
+			if (riskobj != null) {
+				device.pressBack();
+				continue;
+			}
+			Thread.sleep(WAIT_TIMEOUT);
+			device.swipe(561, 1000, 498, 800, 64);
+			Thread.sleep(WAIT_TIMEOUT);
+		}
+		device.pressBack();
+	}
 
-    private void ClickTask(UiObject2 obj) throws Exception {
-        obj.click();
-        Thread.sleep(WAIT_TIMEOUT);
-        device.click(561, GO_FINISH_Y);
-        Thread.sleep(18000);
-        device.pressBack();
-    }
+	private void ClickTask(UiObject2 obj) throws Exception {
+		obj.click();
+		Thread.sleep(WAIT_TIMEOUT);
+		device.click(561, GO_FINISH_Y);
+		Thread.sleep(18000);
+		device.pressBack();
+	}
 
-    private void ShortTVTask(UiObject2 obj) throws Exception {
-        obj.click();
-        Thread.sleep(WAIT_TIMEOUT);
-        device.click(561, GO_FINISH_Y);
-        Thread.sleep(WAIT_TIMEOUT);
+	private void ShortTVTask(UiObject2 obj) throws Exception {
+		obj.click();
+		Thread.sleep(WAIT_TIMEOUT);
+		device.click(561, GO_FINISH_Y);
 
-        for (int j = 0; j < 20; j++) {
-            Thread.sleep(20000);
-            UiObject2 riskobj = device.findObject(By.text("网络不太顺畅"));
-            if (riskobj != null) {
-                device.pressBack();
-                continue;
-            }else{
-                device.click(852,DEVICE_NAME.equals("umi") ? 280 : DEVICE_NAME.equals("ruben") ? 360 : 360);
-            }
-        }
-        Thread.sleep(WAIT_TIMEOUT);
-        device.pressBack();
-    }
+		Thread.sleep(330000);
 
-    @Test
-    public void testAlipaySignIn() throws Exception {
+		device.pressBack();
+	}
 
-        ArrayList<String> blacklist = new ArrayList<>();
-        for (int i = 0; i < LOOP_COUNT; i++) {
-            logger("Loop iteration: " + (i + 1));
-            boolean isClicked = false;
-            seekAndClick("赚更多积分");
-            List<UiObject2> objs = device.findObjects(By.clazz("android.widget.TextView"));
-            if (objs != null && !objs.isEmpty()) {
-                for (UiObject2 obj : objs) {
-                    try {
-                        String text = obj.getText();
-                        if (text == null || text.isEmpty())
-                            continue;
-                        for (String black : blacklist) {
-                            if (text.equals(black)) {
-                                continue;
-                            }
-                        }
+	@Test
+	public void testAlipaySignIn() throws Exception {
 
-                        for (String scrollText : scrollTexts) {
-                            if (text.contains(scrollText)) {
-                                ScrollTask(obj);
-                                isClicked = true;
-                                break;
-                            }
-                        }
-                        if (isClicked)
-                            break;
+		ArrayList<String> blacklist = new ArrayList<>();
+		for (int i = 0; i < LOOP_COUNT; i++) {
+			logger("Loop iteration: " + (i + 1));
+			boolean isClicked = false;
+			seekAndClick("赚更多积分");
+			List<UiObject2> objs = device.findObjects(By.clazz("android.widget.TextView"));
+			if (objs != null && !objs.isEmpty()) {
+				for (UiObject2 obj : objs) {
+					try {
+						String text = obj.getText();
+						if (text == null || text.isEmpty())
+							continue;
+						for (String black : blacklist) {
+							if (text.equals(black)) {
+								continue;
+							}
+						}
 
-                        for (String clickText : clickTexts) {
-                            if (text.equals(clickText)) {
-                                ClickTask(obj);
-                                isClicked = true;
-                                break;
-                            }
-                        }
-                        if (isClicked)
-                            break;
+						for (String scrollText : scrollTexts) {
+							if (text.contains(scrollText)) {
+								ScrollTask(obj);
+								isClicked = true;
+								break;
+							}
+						}
+						if (isClicked)
+							break;
 
-                        if (text.endsWith("5分钟")) {
-                            ShortTVTask(obj);
-                            isClicked = true;
-                            break;
-                        }
-                        if (isClicked)
-                            break;
+						for (String clickText : clickTexts) {
+							if (text.equals(clickText)) {
+								ClickTask(obj);
+								isClicked = true;
+								break;
+							}
+						}
+						if (isClicked)
+							break;
 
-                        if (DEVICE_NAME.equals("umi")) {
-                            for (String awayBackText : awayBackTexts) {
-                                if (text.contains(awayBackText)) {
-                                    obj.click();
-                                    isClicked = true;
-                                    Thread.sleep(WAIT_TIMEOUT);
-                                    // blacklist.add(text);
-                                    device.click(561, GO_FINISH_Y);
-                                    Thread.sleep(8888);
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEEP_LINK_URL));
-                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                    context.startActivity(intent);
-                                    break;
-                                }
-                            }
-                            if (isClicked)
-                                break;
-                        }
+						if (text.startsWith("玩一玩")) {
+							obj.click();
+							isClicked = true;
+							Thread.sleep(WAIT_TIMEOUT);
+							device.click(561, GO_FINISH_Y);
+							Thread.sleep(200000);
+							Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEEP_LINK_URL));
+							intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							context.startActivity(intent);
+							break;
+						}
+						if (isClicked)
+							break;
 
-                    } catch (StaleObjectException e) {
-                        logger("StaleObjectException, skipping...");
-                        continue;
-                    }
-                }
-            }
+						if (text.endsWith("5分钟")) {
+							ShortTVTask(obj);
+							isClicked = true;
+							break;
+						}
+						if (isClicked)
+							break;
 
-            if (isClicked)
-                continue;
-            logger("No task found, checking for 换一换...");
-            justChange();
-        }
-    }
+						if (DEVICE_NAME.equals("umi")) {
+							for (String awayBackText : awayBackTexts) {
+								if (text.contains(awayBackText)) {
+									obj.click();
+									isClicked = true;
+									Thread.sleep(WAIT_TIMEOUT);
+									// blacklist.add(text);
+									device.click(561, GO_FINISH_Y);
+									Thread.sleep(8888);
+									Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(DEEP_LINK_URL));
+									intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+									context.startActivity(intent);
+									break;
+								}
+							}
+							if (isClicked)
+								break;
+						}
 
-    /**
-     * 日志输出
-     */
-    private void logger(String msg) {
-        System.out.println("[AlipaySignIn113] " + msg);
-    }
+					} catch (StaleObjectException e) {
+						logger("StaleObjectException, skipping...");
+						continue;
+					}
+				}
+			}
+
+			if (isClicked)
+				continue;
+			logger("No task found, checking for 换一换...");
+			justChange();
+		}
+	}
+
+	/**
+	 * 日志输出
+	 */
+	private void logger(String msg) {
+		System.out.println("[AlipaySignIn113] " + msg);
+	}
 }
